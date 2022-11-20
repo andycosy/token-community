a12d953d8818752553daaaa3f3092556d8dd9726
山腰伸彦 <>
2022-11-20 13:23:25 +0900

    TokenBank:イベント定義

---
+++ contracts/TokenBank.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract TokenBank {
+    /// @dev Tokenの名前
+    string private _name;
+
+    /// @dev Tokenシンボル
+    string private _symbol;
+
+    /// @dev Tokenの総供給数
+    uint256 constant _totalSupply = 1000;
+
+    /// @dev TokenBankが預かっているTokenの総額
+    uint256 private _bankTotalDeposit;
+
+    /// @dev TokenBankのオーナー
+    address public owner;
+
+    /// @dev アカウントアドレス毎のToken残高
+    mapping(address => uint256) private _balances;
+
+    /// @dev TokenBankが預かっているToken残高
+    mapping(address => uint256) private _tokenBankBalances;
+
+    /// @dev Token移転時のイベント
+    event TokenTransfer(
+        address indexed from,
+        address indexed to,
+        uint256 amount
+    );
+
+    /// @dev Token預け入れ時のイベント
+    event TokenDeposit(
+        address indexed from,
+        uint256 amount
+    );
+
+    /// @dev Token引き出し時のイベント
+    event TokenWithdraw(
+        address indexed from,
+        uint256 amount
+    );
+}
